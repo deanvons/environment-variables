bcf6bf207104cd578510e6cc7108b6c8b7649b22
deanvons <deanvons@gmail.com>
2024-10-03 13:59:28 +0200

    chore: added yaml

--- test.js
+++ test.js
@@ -1,2 +1,12 @@
 require("dotenv/config");
 
+function test(expectedSecret) {
+    const secretMatches = process.env.MEANING_OF_LIFE == expectedSecret;
+    console.log(`The meaning of life is ${process.env.MEANING_OF_LIFE}`);
+    console.log(`The variable is ${process.env.VARIABLE}`);
+    console.log(`The secret is ${process.env.SECRET}`);
+    console.log(`The secret does${secretMatches ? "" : " not"} match!`);
+    return secretMatches;
+  }
+  
+  test(42)
